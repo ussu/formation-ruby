7a8d7ed7b65a0afd8b836e4489c82b8d6cbac987
Sulidi Maimaitiming <sulidi.maimaitiming.ext@qonto.com>
2021-12-27 16:13:29 +0100

    Fizzbuzz

---
+++ katas/lib/fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+  case
+  when number % 15 == 0 then 'fizzbuzz'
+  when number % 3 == 0 then 'fizz'
+  when number % 5 == 0 then 'buzz'
+  else number.to_s
+  end
+end
+
--- katas/spec/fizzbuzz_spec.rb
+++ katas/spec/fizzbuzz_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 require 'fizzbuzz'
 
 describe '#fizzbuzz' do
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq '1'
+  end
+
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
