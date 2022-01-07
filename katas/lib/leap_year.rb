16c1a8d21b90023758cfdb54573d376239b1be01
Sulidi Maimaitiming <sulidi.maimaitiming.ext@qonto.com>
2022-01-07 14:24:28 +0100

    Leap year in functional style

--- katas/lib/leap_year.rb
+++ katas/lib/leap_year.rb
@@ -1,19 +1,15 @@
-# calculates if a year is a leap year
 
 def leap_year?(year)
-  if year.divisible(400)
+  isDivisible = -> (divided, divisor) { divided % divisor == 0 }
+  yearIsDivisibleBy = isDivisible.curry.(year)
+
+  if yearIsDivisibleBy.(400)
     true
-  elsif year.divisible(100)
+  elsif yearIsDivisibleBy.(100)
     false
-  elsif year.divisible(4)
+  elsif yearIsDivisibleBy.(4)
     true
   else
     false
-  end
-end
-
-class Integer
-  def divisible(divisor)
-    self % divisor == 0
   end
 end
